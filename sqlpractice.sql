4b1d30c82d9ef0c87c1af3db4d75498cd9d5a0ea
Ann Maria George <52249835+annchirackal@users.noreply.github.com>
2025-02-06 07:21:08 -0500

    Create sqlpractice.sql
    Day 1 : 20 min medium  - topic joins

---
+++ sqlpractice.sql
@@ -0,0 +1,13 @@
+/*
+- start @6.56 am 
+- end @ 7.15 am
+- new : select with out using join
+*/
+select  c.company_code,c.founder,
+(select count(*) from Lead_Manager where company_code=c.company_code  ) ,
+(select count(*) from Senior_Manager where company_code=c.company_code  ),
+(select count(*) from Manager where company_code=c.company_code  ),
+(select count(*) from Employee  where company_code=c.company_code  )
+
+
+from  Company as c order by c.company_code;
